45e61d6c80b94f23cfbb2c5ab2db2dcc81e89827
MathFreitas <matheus.freitas_santos@outlook.com>
2022-01-10 17:07:07 -0300

    criando um componente prático com estado

--- aula-hooks/src/Components/App/App.jsx
+++ aula-hooks/src/Components/App/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import Counter from '../Counter/Counter';
 import SmartCounter from '../Smartcounter/Smartcounter';
+import Deliverycounter from '../Deliverycounter/Deliverycounter';
 
 
 function App() {
   return (
     <>
       <h1>Hello world</h1>
-      <SmartCounter/>
+      <Deliverycounter/>
     </>
   );
 }
